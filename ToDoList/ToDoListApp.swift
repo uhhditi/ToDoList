79a7e7d0d55f16c0d92fdae6020d39908064276f
aditi <Adishanu4u@gmail.com>
2025-04-27 20:05:06 -0700

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Aditi Agarwal on 4/27/25.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
